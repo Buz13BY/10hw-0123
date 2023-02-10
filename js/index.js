f7013580fa39a6d7a4f9520df3505fe464b3b947
Vlad Tkatsevich work <V.Tkatsevich@gmail.com>
2023-02-10 13:28:56 +0300

    header burger done hearo button and swipre fixed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
-var swiper = new Swiper(".mySwiper", {
-	spaceBetween: 20,
-	pagination: {
-	  el: ".swiper-pagination",
-	  clickable: true,
-	},
-  });
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("burger").addEventListener("click", function(){
+        document.querySelector(".header").classList.toggle("open")
+    })
+	var swiper = new Swiper(".mySwiper", {
+		spaceBetween: 20,
+		pagination: {
+		  el: ".swiper-pagination",
+		  clickable: true,
+		},
+	  });
+})
